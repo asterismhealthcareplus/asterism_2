d918a4019c5fad9a350722d2ee52ccc1989cbf69
Philip <supertaru@gmail.com>
2014-02-26 17:14:22 +0900

    Add styles and icons to product status page

---
+++ app/helpers/static_pages_helper.rb
@@ -0,0 +1,38 @@
+module StaticPagesHelper
+
+	def concept_icon
+		image_tag("concept72.png", alt: "concept icon")
+	end
+
+	def negotiate_icon
+		image_tag("negotiate72.png", alt: "negotiating icon")
+	end
+
+	def agreement_icon
+		image_tag("agreement72.png", alt: "agreement icon")
+	end
+
+	def review_icon
+		image_tag("regulatory_review72.png", alt: "regulatory review icon")
+	end
+
+	def formulate_icon
+		image_tag("formulation72.png", alt: "formulation icon")
+	end
+
+	def sample_icon
+		image_tag("sampling72.png", alt: "sampling icon")
+	end
+
+	def register_icon
+		image_tag("registration72.png", alt: "registration icon")
+	end
+
+	def manufacture_icon
+		image_tag("production72.png", alt: "production icon")
+	end
+
+	def logistics_icon
+		image_tag("logistics72.png", alt: "logistics icon")
+	end
+end
