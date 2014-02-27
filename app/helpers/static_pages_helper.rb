6c7c8f59712d9465a93d32d696c2219081147e7f
Philip <supertaru@gmail.com>
2014-02-27 14:07:22 +0900

    Resized table icons from 72 to 48px and changed body's font-family to Avenir Light

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,38 +1,38 @@
 module StaticPagesHelper
 
 	def concept_icon
-		image_tag("concept72.png", alt: "concept icon")
+		image_tag("concept72.png", class: "table_icon", alt: "concept icon")
 	end
 
 	def negotiate_icon
-		image_tag("negotiate72.png", alt: "negotiating icon")
+		image_tag("negotiate72.png", class: "table_icon", alt: "negotiating icon")
 	end
 
 	def agreement_icon
-		image_tag("agreement72.png", alt: "agreement icon")
+		image_tag("agreement72.png", class: "table_icon", alt: "agreement icon")
 	end
 
 	def review_icon
-		image_tag("regulatory_review72.png", alt: "regulatory review icon")
+		image_tag("regulatory_review72.png", class: "table_icon", alt: "regulatory review icon")
 	end
 
 	def formulate_icon
-		image_tag("formulation72.png", alt: "formulation icon")
+		image_tag("formulation72.png", class: "table_icon", alt: "formulation icon")
 	end
 
 	def sample_icon
-		image_tag("sampling72.png", alt: "sampling icon")
+		image_tag("sampling72.png", class: "table_icon", alt: "sampling icon")
 	end
 
 	def register_icon
-		image_tag("registration72.png", alt: "registration icon")
+		image_tag("registration72.png", class: "table_icon", alt: "registration icon")
 	end
 
 	def manufacture_icon
-		image_tag("production72.png", alt: "production icon")
+		image_tag("production72.png", class: "table_icon", alt: "production icon")
 	end
 
 	def logistics_icon
-		image_tag("logistics72.png", alt: "logistics icon")
+		image_tag("logistics72.png", class: "table_icon", alt: "logistics icon")
 	end
 end
