6fc5729a868caeac9ed40ce10892bba87f93edf0
Philip <supertaru@gmail.com>
2014-03-03 15:00:00 +0900

    Add after_sign_in_path to product_status_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Redirect to product_status_path after user signs in
+  def after_sign_in_path_for user
+  	product_status_path
+  end
 end
