9d9ceb8c1a584add18147f90c9a5cd415eea7d82
Philip <supertaru@gmail.com>
2014-03-10 15:40:15 +0900

    Add error notification to registration form. Add strong parameters to required registration fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :company, presence: true
+  validates :office_location, presence: true
+  validates :office_country, presence: true
+  validates :office_city, presence: true
+  
 end
