ff54875e3c37a6840484627d30836e5da531f2df
Philip Nguyen <supertaru@gmail.com>
2014-03-27 18:13:52 +0900

    Add strong params for update in user_controller.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -26,7 +26,7 @@ class UserController < ApplicationController
     @user = User.find(params[:id])
     params[:user].delete(:password) if params[:user][:password].blank?
     params[:user].delete(:password_confirmation) if params[:user][:password].blank? and params[:user][:password_confirmation].blank?
-    if @user.update_attributes(params[:user])
+    if @user.update_attributes(update_params)
       flash[:notice] = "Successfully updated User."
       redirect_to root_path
     else
@@ -41,5 +41,10 @@ class UserController < ApplicationController
       redirect_to root_path
     end
   end
+
+  private
    
+  def update_params
+    params.require(:user).permit(:company, :first_name, :last_name, :office_location, :office_city, :office_country) 
+  end
 end
